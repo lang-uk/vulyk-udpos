14eb16bd864320ce32ef0302d01338fda819cc1c
Dmitry Chaplinsky <chaplinsky.dmitry@gmail.com>
2026-02-06 01:10:49 +0200

    Changing redundancy

--- vulyk_udpos/models/task_types.py
+++ vulyk_udpos/models/task_types.py
@@ -10,26 +10,31 @@ class UDPOSTaggingTaskType(AbstractTaskType):
     """
     UDPos Tagging Task to work with Vulyk.
     """
+
     answer_model = UDPOSTaggingAnswer
     task_model = UDPOSTaggingTask
 
-    name = 'Розмітка частин мови (UDPOS)'
-    description = 'Тегування частин мови за універсальною залежнісною граматикою (Universal Dependencies).'
+    name = "Розмітка частин мови (UDPOS)"
+    description = "Тегування частин мови за універсальною залежнісною граматикою (Universal Dependencies)."
 
-    template = 'base.html'
-    helptext_template = 'help.html'
-    type_name = 'udpos_tagging_task'
+    template = "base.html"
+    helptext_template = "help.html"
+    type_name = "udpos_tagging_task"
 
-    redundancy = 3
+    redundancy = 1
 
-    JS_ASSETS = ['static/scripts/keymaster.js',
-                 'static/scripts/handlebars.js',
-                 'static/scripts/bootstrap-select.js',
-                 'static/scripts/typeahead.js',
-                 'static/scripts/bootstrap-tagsinput.js',
-                 'static/scripts/base.js']
+    JS_ASSETS = [
+        "static/scripts/keymaster.js",
+        "static/scripts/handlebars.js",
+        "static/scripts/bootstrap-select.js",
+        "static/scripts/typeahead.js",
+        "static/scripts/bootstrap-tagsinput.js",
+        "static/scripts/base.js",
+    ]
 
-    CSS_ASSETS = ['static/styles/bootstrap-select.css',
-                  'static/styles/bootstrap-tagsinput.css',
-                  'static/styles/base.css',
-                  'static/styles/autocomplete.css']
+    CSS_ASSETS = [
+        "static/styles/bootstrap-select.css",
+        "static/styles/bootstrap-tagsinput.css",
+        "static/styles/base.css",
+        "static/styles/autocomplete.css",
+    ]
